d6f2750fb408fab50ea007acc39ebfcd044587c5
alihanafi17 <alihanafi1720@gmail.com>
2025-05-19 14:48:11 +0300

    add css to forgot password

--- ReactArticles/src/components/forgotPassword/ForgotPassword.jsx
+++ ReactArticles/src/components/forgotPassword/ForgotPassword.jsx
@@ -6,6 +6,8 @@ function ForgotPassword() {
   const [email, setEmail] = useState("");
   const [password1, setPassword1] = useState("");
   const [password2, setPassword2] = useState("");
+  const [showPassword1, setShowPassword1] = useState(false);
+  const [showPassword2, setShowPassword2] = useState(false);
   const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
@@ -43,37 +45,86 @@ function ForgotPassword() {
   };
 
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="email">your Email</label>
-        <input
-          id="email"
-          type="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          required
-          placeholder="Enter your email"
-        />
-        <label htmlFor="password">your new Password</label>
-        <input
-          id="password1"
-          type="text"
-          value={password1}
-          onChange={(e) => setPassword1(e.target.value)}
-          required
-          placeholder="Enter new password"
-        />
-        <label htmlFor="password">your new Password again</label>
-        <input
-          id="password2"
-          type="text"
-          value={password2}
-          onChange={(e) => setPassword2(e.target.value)}
-          required
-          placeholder="Enter new password again"
-        />
-        <button type="submit">Change password</button>
-      </form>
+    <div className={classes.loginContainer}>
+      <div className={classes.loginCard}>
+        <h2 className={classes.loginTitle}>Reset Password</h2>
+
+        <form onSubmit={handleSubmit} className={classes.loginForm}>
+          <div className={classes.formGroup}>
+            <label htmlFor="email" className={classes.formLabel}>
+              Email
+            </label>
+            <input
+              id="email"
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+              className={classes.formInput}
+              placeholder="Enter your email"
+            />
+          </div>
+
+          <div className={classes.formGroup}>
+            <label htmlFor="password1" className={classes.formLabel}>
+              New Password
+            </label>
+            <div className={classes.passwordContainer}>
+              <input
+                id="password1"
+                type={showPassword1 ? "text" : "password"}
+                value={password1}
+                onChange={(e) => setPassword1(e.target.value)}
+                required
+                className={classes.formInput}
+                placeholder="Enter new password"
+              />
+              <img
+                src="../../assets/img/hide_password_eye.png"
+                alt="Toggle password visibility"
+                onClick={() => setShowPassword1(!showPassword1)}
+                className={classes.passwordIcon}
+              />
+            </div>
+          </div>
+
+          <div className={classes.formGroup}>
+            <label htmlFor="password2" className={classes.formLabel}>
+              Confirm New Password
+            </label>
+            <div className={classes.passwordContainer}>
+              <input
+                id="password2"
+                type={showPassword2 ? "text" : "password"}
+                value={password2}
+                onChange={(e) => setPassword2(e.target.value)}
+                required
+                className={classes.formInput}
+                placeholder="Confirm new password"
+              />
+              <img
+                src="../../assets/img/hide_password_eye.png"
+                alt="Toggle password visibility"
+                onClick={() => setShowPassword2(!showPassword2)}
+                className={classes.passwordIcon}
+              />
+            </div>
+          </div>
+
+          <div className={classes.formActions}>
+            <button type="submit" className={classes.loginButton}>
+              Reset Password
+            </button>
+            <button
+              type="button"
+              className={classes.forgotButton}
+              onClick={() => navigate("/login")}
+            >
+              Back to Login
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
